d84e1c24f21ace4e8235d9c4281bcb900fe2e2da
mattzarella <frugal.exempt.0r@icloud.com>
2022-06-09 09:34:05 -0700

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,37 +1,66 @@
-let img;
-
-let img_w=400;
-let img_h=630;
-
 let canvas_w=1440;
 let canvas_h=720;
 
-var spot = {
-  x: 100,
-  y: 50
-}
 function preload() {
-  img = loadImage('img/last_best_hope.png');
+  img_1 = loadImage("img/car40.png");
+
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
 
 function setup() {
   let myCanvas = createCanvas(windowWidth, windowHeight);
   myCanvas.parent("main_section");
-  background('yellow');
-  image(img, (canvas_w-img_w)/2,(canvas_h-img_h)/2, img_w, img_h);
+  background(color(12,20,45));
+  textFont('Courier');
+  textStyle("bold");
+  textSize(250);
+  fill(255, 255, 255);
+  text("This",10,275,)
+  text("is",650,275,)
+
+  colorMode(RGB,255);
+  strokeWeight(7);
+  stroke(color(255,0,0));
+  line(1000,175,1150,50);
+  stroke(color(0,255,0));
+  line(1000+12,175+12,1150+12,50+12);
+  stroke(color(0,0,255)); 
+  line(1000+24,175+24,1150+24,50+24);
+
+  strokeWeight(7);
+  stroke(color(255,0,0));
+  line(1000,175,1190,175);
+  stroke(color(0,255,0));
+  line(1000+12,175+12,1190+12,175+12);  
+  stroke(color(0,0,255)); 
+  line(1000+24,175+24,1190+24,175+24);
+
+  strokeWeight(7);
+  stroke(color(255,0,0));
+  line(1157,275,1157,50);
+  stroke(color(0,255,0));
+  line(1157+12,275+12,1157+12,50+12)
+  stroke(color(0,0,255)); 
+  line(1157+24,275+24,1157+24,50+24)
   
+
+  colorMode(RGB,255);
+  strokeWeight(7);
+  stroke(color(255,0,0));
+  noFill();
+  ellipse(1300,175,150,250);
+  stroke(color(0,255,0));
+  ellipse(1300+15,175,150,250);
+  stroke(color(0,0,255)); 
+  ellipse(1300+30,175,150,250);
+
 }
-  
+
   function draw() {
     stroke("black");
     fill('black');
-    spot.x = random(0, width)
-    spot.y = random(0, height)
-    ellipse(spot.x, spot.y, 30, 4, 0)
+    image(img_1, 000,canvas_h-350,canvas_w,500);
 
-  }
+    
+}
 
